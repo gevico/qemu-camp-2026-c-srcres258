029aa7a8126646c84d977e63b88688cc2fcd2484
srcres258 <src.res.211@gmail.com>
2026-04-05 10:53:34 +0800

    Finish exercise 17
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- exercises/17_myfile/17_myfile.c
+++ exercises/17_myfile/17_myfile.c
@@ -8,8 +8,18 @@
 void print_elf_type(uint16_t e_type) {
   const char *type_str;
   switch (e_type) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    case ET_REL:
+      type_str = "Relocatable";
+      break;
+    case ET_EXEC:
+      type_str = "Executable";
+      break;
+    case ET_DYN:
+      type_str = "Shared Object/PIE";
+      break;
+    default:
+      type_str = "Unknown";
+      break;
   }
   printf("ELF Type: %s (0x%x)\n", type_str, e_type);
 }
@@ -23,8 +33,23 @@ int main(int argc, char *argv[]) {
   int fd;
   Elf64_Ehdr ehdr;
 
-  // TODO: 在这里添加你的代码
-  // I AM NOT DONE
-  
+  for (int i = 0; i < 2; i++) {
+    fd = open(filepath[i], O_RDONLY);
+    if (fd < 0) {
+      continue;
+    }
+    if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) {
+      close(fd);
+      continue;
+    }
+    if (ehdr.e_ident[EI_MAG0] == ELFMAG0 &&
+        ehdr.e_ident[EI_MAG1] == ELFMAG1 &&
+        ehdr.e_ident[EI_MAG2] == ELFMAG2 &&
+        ehdr.e_ident[EI_MAG3] == ELFMAG3) {
+      print_elf_type(ehdr.e_type);
+    }
+    close(fd);
+  }
+   
   return 0;
 }
