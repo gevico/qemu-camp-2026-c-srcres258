54020e59c64d3102e32cf71597e166309cfba3f4
srcres258 <src.res.211@gmail.com>
2026-04-05 10:44:55 +0800

    Finish exercise 15
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- exercises/15_url_parser/15_url_parser.c
+++ exercises/15_url_parser/15_url_parser.c
@@ -12,8 +12,27 @@
 int parse_url(const char* url) {
     int err = 0;
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    const char* query_start = strchr(url, '?');
+    if (query_start == NULL) {
+        goto exit;
+    }
+    query_start++;
+
+    char query_buf[1024];
+    strncpy(query_buf, query_start, sizeof(query_buf) - 1);
+    query_buf[sizeof(query_buf) - 1] = '\0';
+
+    char* param = strtok(query_buf, "&");
+    while (param != NULL) {
+        char* equal_pos = strchr(param, '=');
+        if (equal_pos != NULL) {
+            *equal_pos = '\0';
+            char* key = param;
+            char* value = equal_pos + 1;
+            printf("key = %s, value = %s\n", key, value);
+        }
+        param = strtok(NULL, "&");
+    }
 
 exit:
     return err;
