5f7a30430620b6a0b8c50cea67d035b98a808f1a
srcres258 <src.res.211@gmail.com>
2026-04-05 10:29:44 +0800

    Finish exercise 11
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- exercises/11_command_interpreter/11_command_interpreter.c
+++ exercises/11_command_interpreter/11_command_interpreter.c
@@ -57,11 +57,26 @@ int shell_parse(char *buf, char *argv[])
 {
     int argc = 0;
     int state = 0;
-    // TODO: 在这里添加你的代码，完成命令行解析
-    // 功能：将输入字符串buf按空格分割成多个参数，存入argv数组
-    // 返回：参数个数argc
-    // 提示：使用状态机的方式处理，注意处理字符串结束符
-    // I AM NOT DONE
+    
+    for (int i = 0; buf[i] != '\0'; i++) {
+        if (argc >= MAX_ARGS) {
+            break;
+        }
+        
+        if (state == 0) {
+            if (buf[i] != ' ') {
+                argv[argc] = &buf[i];
+                argc++;
+                state = 1;
+            }
+        } else {
+            if (buf[i] == ' ') {
+                buf[i] = '\0';
+                state = 0;
+            }
+        }
+    }
+    
     return argc;
 }
 
