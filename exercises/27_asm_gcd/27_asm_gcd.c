e40de222bb49c398c34840976d5c3b5a9dff6706
srcres258 <src.res.211@gmail.com>
2026-04-10 13:12:48 +0800

    Finish exercise 27
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- exercises/27_asm_gcd/27_asm_gcd.c
+++ exercises/27_asm_gcd/27_asm_gcd.c
@@ -2,27 +2,25 @@
 
 unsigned int gcd_asm(unsigned int a, unsigned int b) {
     unsigned int result;
-    
-    // TODO: 替换下方的TODO段
-    // I AM NOT DONE
+
     __asm__ volatile (
-        "mov %1, %%eax\n\t"     // 将a存入eax
-        "mov %2, %%ebx\n\t"     // 将b存入ebx
-        "jmp .L_check\n\t"      // 跳转到检查条件
-        
-        ".L_loop:\n\t"
-        "   TODO\n\t"
-        
-        ".L_check:\n\t"
-        "   test TODO\n\t"      // 测试ebx是否为0
-        "   jne .L_loop\n\t"    // 如果不为0，继续循环
-        
-        "mov %%eax, %0"         // 将结果（eax）存入输出变量
-        : "=r" (TODO)           // 输出操作数
-        : "r" (TODO), "r" (TODO)      // 输入操作数
-        : "TODO", "TODO", "TODO" // 被修改的寄存器
+        "mov %1, %%eax\n\t"
+        "mov %2, %%ebx\n\t"
+        "1:\n\t"
+        "test %%ebx, %%ebx\n\t"
+        "je 2f\n\t"
+        "xor %%edx, %%edx\n\t"
+        "div %%ebx\n\t"
+        "mov %%ebx, %%eax\n\t"
+        "mov %%edx, %%ebx\n\t"
+        "jmp 1b\n\t"
+        "2:\n\t"
+        "mov %%eax, %0"
+        : "=r"(result)
+        : "r"(a), "r"(b)
+        : "eax", "ebx", "edx", "cc"
     );
-    
+
     return result;
 }
 
